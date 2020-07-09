c59e2b4d6592db296f6a1929557322eb449eb16b
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-07-09 08:25:33 -0700

    Expressjs module completed.

--- Expressjs/app.js
+++ Expressjs/app.js
@@ -1,16 +1,21 @@
+const path = require("path");
+
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("lalal");
-  next();
-});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log("lalal 2");
-  //   send response
-  res.send("<h1>Hello From Express</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
-// const server = http.createServer(app);
-// server.listen(3000);
+
 app.listen(3000);
